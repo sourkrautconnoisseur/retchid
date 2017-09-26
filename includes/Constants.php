45ceb9c624868f7d36ff4b1cc36cf280038aac27
James Krause <32291283+sourkrautconnoisseur@users.noreply.github.com>
2017-09-26 03:39:46 -0400

    Committed Constants.php
    Defaults for use in a development environment.

---
+++ includes/Constants.php
@@ -0,0 +1,19 @@
+<?php
+
+const SQL_CONNECTION_ARRAY =  array(
+	'HOST' => '',
+	'USERNAME' => '',
+	'PASSWORD' => '',
+	'DATABASE' => ''
+	 );
+
+
+const DEBUG_VARIABLES =  array(
+	'JS' => true,
+	'PHP' => true
+	 );
+
+
+
+
+?>
